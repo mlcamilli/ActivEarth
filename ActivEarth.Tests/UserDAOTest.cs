3a4f38b364eafcc3aeb8ffdd5ff284370c2e9aba
Matt Camilli <matt.camilli@excella.com>
2012-02-23 16:24:29 -0500

    maybe this will fix it

--- ActivEarth.Tests/UserDAOTest.cs
+++ ActivEarth.Tests/UserDAOTest.cs
@@ -8,53 +8,54 @@ using ActivEarth.Objects;
 using ActivEarth.Server.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace ActivEarth.Tests
 {
     
     [TestClass]
     public class UserDAOTest
     {
-//        private TransactionScope _trans;
-//        public TestContext TestContext { get; set; }
-//        [ClassInitialize]
-//        public static void ClassInitialize(TestContext testContext)
-//        {
-//            ConnectionManager.ConnectionString =
-//                "data source=venus.cs.vt.edu;Initial Catalog=ActivEarth_Dev;user id = aeadmin; password = Hansh0tfirst;";
-//        }
-//        
-//        [TestInitialize]
-//        public void Initialize()
-//        {
-//            _trans = new TransactionScope();
-//            
-//        }
-//        [TestCleanup]
-//        public void CleanUp()
-//        {
-//            _trans.Dispose();
-//        }
-//
-//        [TestMethod]
-//        public void TestConfirmPassword()
-//        {
-//            var user = new User
-//            {
-//                UserName = "testy123",
-//                Age = 25,
-//                City = "Bleaksburg",
-//                Email = "whatisthis@idont.even",
-//                FirstName = "I.C.",
-//                Gender = 'M',
-//                Height = 60,
-//                LastName = "Poorcode",
-//                State = "VA",
-//                Weight = 130
-//            };
-//            UserDAO.CreateNewUser(user, "test");
-//            Assert.IsTrue(UserDAO.ConfirmPassword("test", UserDAO.GetUserIdFromUserName("testy123")));
-//            
-//        }
+        private TransactionScope _trans;
+        public TestContext TestContext { get; set; }
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            ConnectionManager.ConnectionString =
+                "data source=venus.cs.vt.edu;Initial Catalog=ActivEarth_Dev;user id = aeadmin; password = Hansh0tfirst;";
+        }
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            _trans = new TransactionScope();
+            
+        }
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _trans.Dispose();
+        }
+
+        [TestMethod]
+        public void TestConfirmPassword()
+        {
+            var user = new User
+            {
+                UserName = "testy123",
+                Age = 25,
+                City = "Bleaksburg",
+                Email = "whatisthis@idont.even",
+                FirstName = "I.C.",
+                Gender = 'M',
+                Height = 60,
+                LastName = "Poorcode",
+                State = "VA",
+                Weight = 130
+            };
+            UserDAO.CreateNewUser(user, "test");
+            Assert.IsTrue(UserDAO.ConfirmPassword("test", UserDAO.GetUserIdFromUserName("testy123")));
+            
+        }
 
     }
 }
