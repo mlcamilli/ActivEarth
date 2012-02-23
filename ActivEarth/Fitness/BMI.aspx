f9fe016ebadb677a86151e1e4d011b1fd681a8ad
Matt Camilli <mlcamilli@gmail.com>
2012-02-23 02:05:41 -0500

    added bmi calculator

---
+++ ActivEarth/Fitness/BMI.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BMI.aspx.cs" Inherits="ActivEarth.Fitness.BMI" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>
+        Your BMI
+    </h2>
+    <asp:Panel runat="server" ID="pnlBMI">
+    <p>
+        Your BMI is <b><asp:Label runat="server" ID="lblBMI"></asp:Label></b> which means you are <asp:Label runat="server" ID="lblBMIResult"></asp:Label>.
+    </p>
+    </asp:Panel>
+    <asp:Panel runat="server" ID="pnlNotEnoughData">
+        <br/>
+        <asp:Label runat="server" Text="You have not updated your profile with all of the necessary fields. Please do so "></asp:Label><asp:HyperLink runat="server" Text="here." NavigateUrl="~/Account/EditProfile.aspx"></asp:HyperLink>
+    </asp:Panel>
+
+</asp:Content>
---
+++ ActivEarth/Fitness/BMI.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ActivEarth.Objects;
+
+namespace ActivEarth.Fitness
+{
+    public partial class BMI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var user = (User)Session["userDetails"];
+            if (user == null)
+            {
+                Response.Redirect("~/Account/Login.aspx");
+            }
+            else if (user.Height == null || user.Weight == null)
+            {
+                pnlBMI.Visible = false;
+                pnlNotEnoughData.Visible = true;
+            }
+            else
+            {
+                pnlBMI.Visible = true;
+                pnlNotEnoughData.Visible = false;
+
+                var bmi = (double) ((user.Weight*703)/(user.Height*user.Height)) ;
+                if (bmi < 18.5)
+                {
+                    lblBMIResult.Text = "underweight";
+                }
+                else if (bmi >= 18.5 && bmi < 25)
+                {
+                    lblBMIResult.Text = "normal";
+                }
+                else if (bmi >= 25 && bmi < 30)
+                {
+                    lblBMIResult.Text = "overweight";
+                }
+                else
+                {
+                    lblBMIResult.Text = "obese";
+                }
+
+                lblBMI.Text = bmi.ToString();
+
+            }
+
+        }
+    }
+}
