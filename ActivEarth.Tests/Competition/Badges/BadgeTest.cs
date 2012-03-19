50dc9a773dbc7f26a0453eb8bb62824a1da747bf
Mark Talley <mtalley1@vt.edu>
2012-03-19 03:48:34 -0400

    Test Code for Badge and Challenge Systems.

---
+++ ActivEarth.Tests/Competition/Badges/BadgeTest.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ActivEarth.Competition;
+using ActivEarth.Competition.Badges;
+
+using Statistics = ActivEarth.Competition.Placeholder.Statistics;
+using User = ActivEarth.Competition.Placeholder.User;
+using Group = ActivEarth.Competition.Placeholder.Group;
+
+namespace ActivEarth.Tests.Competition.Badges
+{
+    /// <summary>
+    /// Test the functionality of the Badge system
+    /// </summary>
+    [TestClass]
+    public class BadgeTest
+    {
+        private Placeholder.User _user;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._user = new User("Test", "Subject");
+
+            this._user.Badges[Statistics.BikeDistance] = 
+                new Badge(1, "BikeDistance", this._user, Statistics.BikeDistance,
+                BadgeConstants.BikingDistance.REQUIREMENTS, BadgeConstants.BikingDistance.POINTS,
+                BadgeConstants.BikingDistance.IMAGES);
+
+            this._user.Badges[Statistics.WalkDistance] =
+                new Badge(2, "WalkDistance", this._user, Statistics.WalkDistance,
+                BadgeConstants.WalkingDistance.REQUIREMENTS, BadgeConstants.WalkingDistance.POINTS,
+                BadgeConstants.WalkingDistance.IMAGES);
+
+            this._user.Badges[Statistics.RunDistance] =
+                new Badge(3, "RunDistance", this._user, Statistics.RunDistance,
+                BadgeConstants.RunningDistance.REQUIREMENTS, BadgeConstants.RunningDistance.POINTS,
+                BadgeConstants.RunningDistance.IMAGES);
+
+            this._user.Badges[Statistics.Steps] =
+                new Badge(4, "Steps", this._user, Statistics.Steps,
+                BadgeConstants.Steps.REQUIREMENTS, BadgeConstants.Steps.POINTS,
+                BadgeConstants.Steps.IMAGES);
+
+            this._user.Badges[Statistics.GasSavings] =
+                new Badge(5, "BikeDistance", this._user, Statistics.GasSavings,
+                BadgeConstants.GasMoney.REQUIREMENTS, BadgeConstants.GasMoney.POINTS,
+                BadgeConstants.GasMoney.IMAGES);
+
+            this._user.Badges[Statistics.ChallengesCompleted] =
+                new Badge(6, "BikeDistance", this._user, Statistics.ChallengesCompleted,
+                BadgeConstants.Challenges.REQUIREMENTS, BadgeConstants.Challenges.POINTS,
+                BadgeConstants.Challenges.IMAGES);
+
+            this._user.SetStatistic(Statistics.BikeDistance, 0);
+            this._user.SetStatistic(Statistics.WalkDistance, 0);
+            this._user.SetStatistic(Statistics.RunDistance, 0);
+            this._user.SetStatistic(Statistics.Steps, 0);
+            this._user.SetStatistic(Statistics.GasSavings, 0);
+            this._user.SetStatistic(Statistics.ChallengesCompleted, 0);
+        }
+
+        [TestMethod]
+        public void TestBadgeUpdateInitialState()
+        {
+            Badge badge = this._user.Badges[Statistics.Steps];
+            Assert.AreEqual(0, badge.Update());
+        }
+
+        [TestMethod]
+        public void TestBadgeUpdateNoChange()
+        {
+            Badge badge = this._user.Badges[Statistics.Steps];
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Bronze]);
+
+            Assert.AreEqual(BadgeConstants.Steps.POINTS[BadgeLevels.Bronze], badge.Update());
+            Assert.AreEqual(0, badge.Update());
+        }
+
+        [TestMethod]
+        public void TestBadgeUpdateUpOneLevel()
+        {
+            Badge badge = this._user.Badges[Statistics.Steps];
+
+            for (int level = BadgeLevels.Bronze; level <= BadgeLevels.Diamond; level++)
+            {
+                this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[level]);
+                Assert.AreEqual(BadgeConstants.Steps.POINTS[level], badge.Update());
+            }
+        }
+
+        [TestMethod]
+        public void TestBadgeUpdateUpMultipleLevels()
+        {
+            Badge badge = this._user.Badges[Statistics.Steps];
+
+            for (int level = BadgeLevels.Bronze; level <= BadgeLevels.Diamond; level+=2)
+            {
+                this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[level]);
+                Assert.AreEqual(BadgeConstants.Steps.POINTS[level] + BadgeConstants.Steps.POINTS[level - 1], badge.Update());
+            }
+        }
+
+        [TestMethod]
+        public void TestBadgeStatisticIndependence()
+        {
+            Badge stepBadge = this._user.Badges[Statistics.Steps];
+            Badge walkBadge = this._user.Badges[Statistics.WalkDistance];
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Bronze]);
+            this._user.SetStatistic(Statistics.WalkDistance, BadgeConstants.WalkingDistance.REQUIREMENTS[BadgeLevels.Bronze]);
+
+            Assert.AreEqual(BadgeConstants.Steps.POINTS[BadgeLevels.Bronze], stepBadge.Update());
+            Assert.AreEqual(BadgeConstants.WalkingDistance.POINTS[BadgeLevels.Bronze], walkBadge.Update());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Silver]);
+
+            Assert.AreEqual(0, walkBadge.Update());
+            Assert.AreEqual(BadgeConstants.Steps.POINTS[BadgeLevels.Silver], stepBadge.Update());
+        }
+
+        [TestMethod]
+        public void TestBadgeGetImagePath()
+        {
+            Badge stepBadge = this._user.Badges[Statistics.Steps];
+            Assert.AreEqual(BadgeConstants.Steps.IMAGES[BadgeLevels.None], stepBadge.GetImagePath());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Bronze]);
+            stepBadge.Update();
+            Assert.AreEqual(BadgeConstants.Steps.IMAGES[BadgeLevels.Bronze], stepBadge.GetImagePath());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Platinum]);
+            stepBadge.Update();
+            Assert.AreEqual(BadgeConstants.Steps.IMAGES[BadgeLevels.Platinum], stepBadge.GetImagePath());
+        }
+
+        [TestMethod]
+        public void TestBadgeGetNextLevelRequirement()
+        {
+            Badge stepBadge = this._user.Badges[Statistics.Steps];
+            Assert.AreEqual(BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Bronze], stepBadge.GetNextLevelRequirement());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Gold]);
+            stepBadge.Update();
+            Assert.AreEqual(BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Platinum], stepBadge.GetNextLevelRequirement());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Max]);
+            stepBadge.Update();
+            Assert.AreEqual(float.PositiveInfinity, stepBadge.GetNextLevelRequirement());
+        }
+
+        [TestMethod]
+        public void TestBadgeGetNextLevelPoints()
+        {
+            Badge stepBadge = this._user.Badges[Statistics.Steps];
+            Assert.AreEqual(BadgeConstants.Steps.POINTS[BadgeLevels.Bronze], stepBadge.GetNextLevelPoints());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Gold]);
+            stepBadge.Update();
+            Assert.AreEqual(BadgeConstants.Steps.POINTS[BadgeLevels.Platinum], stepBadge.GetNextLevelPoints());
+
+            this._user.SetStatistic(Statistics.Steps, BadgeConstants.Steps.REQUIREMENTS[BadgeLevels.Max]);
+            stepBadge.Update();
+            Assert.AreEqual(0, stepBadge.GetNextLevelPoints());
+        }
+    }
+}
---
+++ ActivEarth.Tests/Competition/Challenges/ChallengeTest.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ActivEarth.Competition;
+using ActivEarth.Competition.Challenges;
+
+using Statistics = ActivEarth.Competition.Placeholder.Statistics;
+using User = ActivEarth.Competition.Placeholder.User;
+using Group = ActivEarth.Competition.Placeholder.Group;
+
+namespace ActivEarth.Tests.Competition.Challenges
+{
+    /// <summary>
+    /// Tests the functionality of the Challenge system.
+    /// </summary>
+    [TestClass]
+    public class ChallengeTest
+    {
+        private User _user1;
+        private User _user2;
+
+        private Group _allUsers;
+
+        private ChallengeManager _manager;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._user1 = new User("Test", "Subject1");
+            this._user2 = new User("Test", "Subject2");
+
+            this._allUsers = new Group("All Users");
+            this._allUsers.Members.Add(this._user1);
+            this._allUsers.Members.Add(this._user2);
+
+            this._manager = new ChallengeManager(this._allUsers);
+        }
+
+        [TestMethod]
+        public void TestChallengeCreation()
+        {
+            this._user2.SetStatistic(Statistics.Steps, 50);
+
+            uint id = this._manager.CreateChallenge("Test Challenge", "This is a test challenge", 
+                30, false, DateTime.Now.AddDays(1), Statistics.Steps, 500);
+
+            Assert.IsTrue(this._user1.ChallengeInitialValues.ContainsKey(id));
+            Assert.AreEqual(0, this._user1.ChallengeInitialValues[id]);
+
+            Assert.IsTrue(this._user2.ChallengeInitialValues.ContainsKey(id));
+            Assert.AreEqual(50, this._user2.ChallengeInitialValues[id]);
+        }
+
+        [TestMethod]
+        public void TestChallengeProgressIncomplete()
+        {
+            this._user2.SetStatistic(Statistics.Steps, 50);
+
+            uint id = this._manager.CreateChallenge("Test Challenge", "This is a test challenge", 
+                30, false, DateTime.Now.AddDays(1), Statistics.Steps, 500);
+
+            Challenge challenge = this._manager.GetChallenge(id);
+
+            this._user1.SetStatistic(Statistics.Steps, 200);
+            this._user2.SetStatistic(Statistics.Steps, 200);
+
+            Assert.AreEqual(200, challenge.GetProgress(this._user1));
+            Assert.AreEqual(150, challenge.GetProgress(this._user2));
+
+            Assert.IsFalse(challenge.IsComplete(this._user1));
+            Assert.IsFalse(challenge.IsComplete(this._user2));
+        }
+
+        [TestMethod]
+        public void TestChallengeProgressComplete()
+        {            
+            this._user2.SetStatistic(Statistics.Steps, 50);
+
+            uint id = this._manager.CreateChallenge("Test Challenge", "This is a test challenge", 
+                30, false, DateTime.Now.AddDays(1), Statistics.Steps, 500);
+
+            Challenge challenge = this._manager.GetChallenge(id);
+
+            this._user1.SetStatistic(Statistics.Steps, 525);
+            this._user2.SetStatistic(Statistics.Steps, 550);
+
+            Assert.AreEqual(500, challenge.GetProgress(this._user1));
+            Assert.AreEqual(500, challenge.GetProgress(this._user2));
+
+            Assert.IsTrue(challenge.IsComplete(this._user1));
+            Assert.IsTrue(challenge.IsComplete(this._user2));
+        }
+        
+        [TestMethod]
+        public void TestChallengeMultipleInitialization()
+        {
+            this._user1.SetStatistic(Statistics.Steps, 50);
+            this._user1.SetStatistic(Statistics.BikeDistance, 100);
+
+            uint id1 = this._manager.CreateChallenge("Test Step Challenge", "This is a test challenge",
+                30, false, DateTime.Now.AddDays(1), Statistics.Steps, 500);
+
+            uint id2 = this._manager.CreateChallenge("Test Bike Challenge", "This is a test challenge",
+                30, false, DateTime.Now.AddDays(1), Statistics.BikeDistance, 100);
+
+            this._user1.SetStatistic(Statistics.Steps, 150);
+
+            uint id3 = this._manager.CreateChallenge("Test Step Challenge 2", "This is another test challenge",
+                30, false, DateTime.Now.AddDays(1), Statistics.Steps, 500);
+
+            Assert.IsTrue(this._user1.ChallengeInitialValues.ContainsKey(id1));
+            Assert.AreEqual(50, this._user1.ChallengeInitialValues[id1]);
+
+            Assert.IsTrue(this._user1.ChallengeInitialValues.ContainsKey(id2));
+            Assert.AreEqual(100, this._user1.ChallengeInitialValues[id2]);
+
+            Assert.IsTrue(this._user1.ChallengeInitialValues.ContainsKey(id3));
+            Assert.AreEqual(150, this._user1.ChallengeInitialValues[id3]);
+        }
+
+        [TestMethod]
+        public void TestChallengeCleanup()
+        {
+            uint id1 = this._manager.CreateChallenge("Test Step Challenge", "This is an expired transient challenge",
+                30, false, DateTime.Now.AddMinutes(-5), Statistics.Steps, 500);
+
+            uint id2 = this._manager.CreateChallenge("Test Bike Challenge", "This is an active transient challenge",
+                30, false, DateTime.Now.AddDays(1), Statistics.BikeDistance, 100);
+
+            uint id3 = this._manager.CreateChallenge("Test Step Challenge 2", "This is a persistent challenge",
+                30, true, DateTime.Now.AddMinutes(-5), Statistics.Steps, 500);
+
+            Assert.IsNotNull(this._manager.GetChallenge(id1));
+            Assert.IsNotNull(this._manager.GetChallenge(id2));
+            Assert.IsNotNull(this._manager.GetChallenge(id3));
+
+            this._manager.CleanUp();
+
+            Assert.IsNull(this._manager.GetChallenge(id1));
+            Assert.IsNotNull(this._manager.GetChallenge(id2));
+            Assert.IsNotNull(this._manager.GetChallenge(id3));
+
+            Assert.IsNotNull(this._manager.GetChallenge(id1, false));
+        }
+
+        [TestMethod]
+        public void TestChallengeResetPersistentChallenge()
+        {
+            uint id = this._manager.CreateChallenge("Test Step Challenge", "This is a persistent challenge",
+                30, true, DateTime.Now.AddMinutes(-5), Statistics.Steps, 500);
+
+            Challenge challenge = this._manager.GetChallenge(id);
+
+            this._user1.SetStatistic(Statistics.Steps, 250);
+            Assert.AreEqual(250, challenge.GetProgress(this._user1));
+
+            this._manager.CleanUp();
+
+            Assert.AreEqual(0, challenge.GetProgress(this._user1));
+            this._user1.SetStatistic(Statistics.Steps, 500);
+            Assert.AreEqual(250, challenge.GetProgress(this._user1));
+        }
+    }
+}
