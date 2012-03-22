bba63e1611243bcaaab78f1d8208c4cf9c053620
Mark Talley <mtalley1@vt.edu>
2012-03-22 02:48:10 -0400

    Added Challenge DAO layer and its associated tests.

---
+++ ActivEarth.Server.Service/DAO/ChallengeDAO.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using ActivEarth.Objects.Competition;
+using ActivEarth.Objects.Competition.Challenges;
+using ActivEarth.Server.Service;
+
+namespace ActivEarth.DAO
+{
+    public class ChallengeDAO
+    {
+        public static Challenge GetChallengeFromChallengeId(uint challengeId)
+        {
+            using (SqlConnection connection = ConnectionManager.GetConnection())
+            {
+                var data = new ActivEarthDataProvidersDataContext(connection);
+                return (from c in data.ChallengeDataProviders
+                        where c.id == challengeId
+                        select
+                            new Challenge
+                            {
+                                ID = (uint)c.id,
+                                Name = c.name,
+                                Description = c.description,
+                                Points = c.points,
+                                Requirement = (float)c.requirement,
+                                IsPersistent = c.persistent,
+                                EndTime = c.end_time,
+                                Duration = new TimeSpan(c.duration_days, 0, 0, 0),
+                                StatisticBinding = (Placeholder.Statistic)c.statistic,
+                                IsActive = c.active
+                            }).FirstOrDefault();
+            }
+        }
+
+        public static bool CreateNewChallenge(Challenge challenge)
+        {
+            try
+            {
+                using (SqlConnection connection = ConnectionManager.GetConnection())
+                {
+                    var data = new ActivEarthDataProvidersDataContext(connection);
+                    var challengeData = new ChallengeDataProvider 
+                        {
+                            id = (int)challenge.ID,
+                            name = challenge.Name,
+                            description = challenge.Description,
+                            points = challenge.Points,
+                            requirement = challenge.Requirement,
+                            persistent = challenge.IsPersistent,
+                            end_time = challenge.EndTime,
+                            duration_days = challenge.Duration.Days,
+                            statistic = (byte)challenge.StatisticBinding,
+                            active = challenge.IsActive
+                        };
+                    data.ChallengeDataProviders.InsertOnSubmit(challengeData);
+                    data.SubmitChanges();
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool UpdateChallenge(Challenge challenge)
+        {
+            try
+            {
+                using (SqlConnection connection = ConnectionManager.GetConnection())
+                {
+                    var data = new ActivEarthDataProvidersDataContext(connection);
+                    ChallengeDataProvider dbChallenge =
+                        (from c in data.ChallengeDataProviders where c.id == challenge.ID select c).FirstOrDefault();
+                    if (dbChallenge != null)
+                    {
+                        dbChallenge.name = challenge.Name;
+                        dbChallenge.description = challenge.Description;
+                        dbChallenge.points = challenge.Points;
+                        dbChallenge.requirement = challenge.Requirement;
+                        dbChallenge.persistent = challenge.IsPersistent;
+                        dbChallenge.end_time = challenge.EndTime;
+                        dbChallenge.duration_days = challenge.Duration.Days;
+                        dbChallenge.statistic = (byte)challenge.StatisticBinding;
+                        dbChallenge.active = challenge.IsActive;
+
+                        data.SubmitChanges();
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ ActivEarth.Tests/Competition/Challenges/ChallengeDAOTest.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using ActivEarth.DAO;
+using ActivEarth.Objects.Competition;
+using ActivEarth.Objects.Competition.Challenges;
+using ActivEarth.Server.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ActivEarth.Tests.Competition.Challenges
+{
+    /// <summary>
+    /// Summary description for ChallengeDAOTest
+    /// </summary>
+    [TestClass]
+    public class ChallengeDAOTest
+    {
+        private TransactionScope _trans;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _trans = new TransactionScope();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            _trans.Dispose();
+        }
+
+        #region ---------- Test Cases ----------
+
+        [TestMethod]
+        public void TestCreateChallenge()
+        {
+            using (_trans)
+            {
+                uint id = 5;
+
+                Log("Creating challenge");
+                Challenge challenge = new Challenge(id, "Test Challenge", "This is a test challenge",
+                    30, false, DateTime.Today, 1, Placeholder.Statistic.Steps, 500);
+
+                Log("Adding challenge to the database");
+                ChallengeDAO.CreateNewChallenge(challenge);
+
+                Log("Loading challenge from the database");
+                Challenge retrieved = ChallengeDAO.GetChallengeFromChallengeId(id);
+
+                Log("Verifying that a matching challenge was found");
+                Assert.IsNotNull(retrieved);
+
+                Log("Verifying the integrity of challenge fields");
+                Assert.AreEqual(challenge.Name, retrieved.Name);
+                Assert.AreEqual(challenge.Description, retrieved.Description);
+                Assert.AreEqual(challenge.EndTime, retrieved.EndTime);
+                Assert.AreEqual(challenge.ID, id);
+                Assert.AreEqual(challenge.IsActive, retrieved.IsActive);
+                Assert.AreEqual(challenge.IsPersistent, retrieved.IsPersistent);
+                Assert.AreEqual(challenge.Points, retrieved.Points);
+                Assert.AreEqual(challenge.StatisticBinding, retrieved.StatisticBinding);
+            }
+        }
+
+        [TestMethod]
+        public void TestUpdateChallenge()
+        {
+            using (_trans)
+            {
+                uint id = 7;
+
+                Log("Creating challenge");
+                Challenge challenge = new Challenge(id, "Test Challenge", "This is a test challenge",
+                    30, false, DateTime.Today, 1, Placeholder.Statistic.Steps, 500);
+
+                Log("Adding challenge to the database");
+                ChallengeDAO.CreateNewChallenge(challenge);
+
+                Log("Loading challenge from the database");
+                Challenge retrieved = ChallengeDAO.GetChallengeFromChallengeId(id);
+
+                Log("Verifying that a matching challenge was found");
+                Assert.IsNotNull(retrieved);
+
+                Log("Verifying that the challenge is still active");
+                Assert.IsTrue(retrieved.IsActive);
+
+                retrieved.IsActive = false;
+
+                Log("Updating the challenge to be expired");
+                ChallengeDAO.UpdateChallenge(retrieved);
+
+                Log("Reloading challenge from the database");
+                Challenge retrieved2 = ChallengeDAO.GetChallengeFromChallengeId(id);
+
+                Log("Verifying that a matching challenge was found");
+                Assert.IsNotNull(retrieved2);
+
+                Log("Verifying that the challenge is inactive");
+                Assert.IsFalse(retrieved2.IsActive);
+
+            }
+        }
+
+        #endregion ---------- Test Cases ----------
+
+        #region ---------- Utility Methods ----------
+
+        /// <summary>
+        /// Logs a message to the Test Context's output (Test Results file).
+        /// </summary>
+        /// <param name="message">Message to add to the test log.</param>
+        private void Log(string message)
+        {
+            TestContext.WriteLine(message);
+        }
+
+        #endregion ---------- Utility Methods ----------
+
+    }
+}
