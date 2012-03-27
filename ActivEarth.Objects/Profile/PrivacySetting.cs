6c7394f67147e0c25b51cc5b21d08fdebce468f8
Qun Zheng <qzheng1989@gmail.com>
2012-03-27 00:08:31 -0400

    added some feature support for profile

---
+++ ActivEarth.Objects/Profile/PrivacySetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActivEarth.Objects.Profile
+{
+    public class PrivacySetting
+    {
+        public ProfileVisibility profileVisibility{ get; set;}
+        public bool email{ get; set; }
+        public bool gender{ get; set;}
+        public bool age{ get; set; }
+        public bool weight{ get; set; }
+        public bool height{ get; set; }
+        public bool location{ get; set; }
+        public bool group{ get; set; }
+    }
+}
---
+++ ActivEarth.Objects/Profile/ProfileVisibility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ActivEarth.Objects.Profile
+{
+    public enum ProfileVisibility
+    {
+        All,
+        AllUser,
+        Group,
+        SelectedGroup,
+        None
+    }
+}
