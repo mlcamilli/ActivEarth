0fb989d06857b1f6599caccc27a09817e3ab9ca7
Qun Zheng <qzheng1989@gmail.com>
2012-04-16 02:56:33 -0400

    adding some master pages

---
+++ ActivEarth/SiteNotLoggedIn.Master.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ActivEarth.Objects;
+using ActivEarth.Objects.Profile;
+
+namespace ActivEarth
+{
+    public partial class SiteNotLoggedIn : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["userDetails"] == null)
+            {
+                lbLogOut.Visible = false;
+                lblUserLoggedIn.Visible = false;
+            }else
+            {
+                var userDetails = (User) Session["userDetails"];
+                hlRegister.Visible = false;
+                hlLogin.Visible = false;
+                lbLogOut.Visible = true;
+                lblUserLoggedIn.Visible = true;
+                lblUserLoggedIn.Text = "Logged in as " + userDetails.UserName;
+
+                
+
+            }
+        }
+        protected void UserLogOut(object sender, EventArgs e)
+        {
+            Session["userDetails"] = null;
+            Response.Redirect("~/Default.aspx");
+        }
+    }
+       
+ 
+}
