9297bdfbb13ccc513e167ac76deb7cc05dddcf5b
Mark Talley <mtalley1@vt.edu>
2012-05-02 21:43:25 -0400

    Removed quotation marks on the homepage around 'Burn Calories, not Carbon'

--- ActivEarth/SiteNotLoggedIn.Master.designer.cs
+++ ActivEarth/SiteNotLoggedIn.Master.designer.cs
@@ -40,6 +40,15 @@ namespace ActivEarth {
         protected global::System.Web.UI.HtmlControls.HtmlForm Form1;
         
         /// <summary>
+        /// Img1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Img1;
+        
+        /// <summary>
         /// lblUserLoggedIn control.
         /// </summary>
         /// <remarks>
