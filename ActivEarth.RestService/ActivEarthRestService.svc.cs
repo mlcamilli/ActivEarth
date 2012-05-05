5f412ac30ebf0ce1fc8e672395d07a6664827c3a
John Young <vt.jyoung@gmail.com>
2012-05-04 23:11:59 -0400

    added new service

---
+++ ActivEarth.RestService/ActivEarthRestService.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ActivEarth.RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ActivEarthRestService" in code, svc and config file together.
+    public class ActivEarthRestService : IActivEarthRestService
+    {
+        public void DoWork()
+        {
+        }
+
+        public string XMLData(string id)
+        {
+            return "You requsted id: " + id;
+        }
+    }
+}
---
+++ ActivEarth.RestService/IActivEarthRestService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ActivEarth.RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IActivEarthRestService" in both code and config file together.
+    [ServiceContract]
+    public interface IActivEarthRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+    }
+}
