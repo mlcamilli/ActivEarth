948a6efe1847c82f7bcc114ec669983a298c23a2
Kyle Akers <ktakers@vt.edu>
2012-05-09 16:15:38 -0400

    Added colored selected tab

--- ActivEarth/Tabs.master.cs
+++ ActivEarth/Tabs.master.cs
@@ -11,7 +11,28 @@ namespace ActivEarth
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string url = Request.Url.ToString();
 
+            if (url.Contains("Account"))
+            {
+                NavigationMenu.Items[0].Selected = true;
+            }
+            else if (url.Contains("Groups"))
+            {
+                NavigationMenu.Items[1].Selected = true;
+            }
+            else if (url.Contains("Badges"))
+            {
+                NavigationMenu.Items[2].Selected = true;
+            }
+            else if (url.Contains("Challenges"))
+            {
+                NavigationMenu.Items[3].Selected = true;
+            }
+            else if (url.Contains("Contests"))
+            {
+                NavigationMenu.Items[4].Selected = true;
+            }
         }
     }
 }
